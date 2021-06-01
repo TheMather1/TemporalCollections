c7989ca3d15fb6a8ae44a9109c4b1d6022b0a761
TheMather1 <the.mather1@gmail.com>
2021-06-01 13:05:37 +0200

    Add publication details.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ java {
 
 publishing {
     repositories {
-        mavenCentral()
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/TheMather1/temporal-collections")
@@ -36,6 +35,31 @@ publishing {
         register("gpr", MavenPublication::class) {
             artifactId = "temporal-collections"
             from(components["java"])
+            pom {
+                name.set("Temporal Collections")
+                description.set("Collections for mapping data by time.")
+                url.set("https://github.com/TheMather1/temporal-collections")
+                inceptionYear.set("2021")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mathias Sand Jahren")
+                        email.set("the.mather1@gmail.com")
+                        url.set("https://github.com/TheMather1")
+                        timezone.set("CET")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/TheMather1/temporal-collections.git")
+                    developerConnection.set("scm:git:ssh://github.com/TheMather1/temporal-collections.git")
+                    url.set("https://github.com/TheMather1/temporal-collections/tree/main")
+                }
+            }
         }
     }
 }
